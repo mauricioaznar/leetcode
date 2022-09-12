84d7389e37a538da8c3fd600c1a9e6143b5fcff0
mauricioaznar <mauricioaznar94@gmail.com>
2022-09-12 17:50:40 -0500

    edited 6.js

--- 6.js
+++ 6.js
@@ -5,7 +5,7 @@
  * @param {number} numRows
  * @return {string}
  */
-var convert = function(s, n) {
+var convert1 = function(s, n) {
 
     let direction = true
 
@@ -27,6 +27,49 @@ var convert = function(s, n) {
             const pos = direction ? n - j - 1 : j
 
             arr[pos].push(s[i])
+        }
+
+
+
+    }
+
+
+    return arr.map(e => e.join('')).join('')
+
+};
+
+// clean solution
+
+/**
+ * @param {string} s
+ * @param {number} numRows
+ * @return {string}
+ */
+var convert2 = function(s, n) {
+    if (s.length === 0 || n === 1) return s
+
+
+    const arr = Array(n).fill(null).map(e => new Array())
+
+    let goingUp = true
+
+    let index = 1
+
+
+    for (let i = 0; i < s.length; i++) {
+        arr[index - 1].push(s[i])
+
+        if (index === n) {
+            goingUp = false
+        } else if (index === 1) {
+            goingUp = true
+        }
+
+
+        if (goingUp) {
+            index = index + 1
+        } else {
+            index = index - 1
         }
 
 
