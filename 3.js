8237bf2383b96c617ed7c1968dc955b8e881fede
mauricioaznar <mauricioaznar94@gmail.com>
2022-09-12 11:00:14 -0500

    edited 3.js

--- 3.js
+++ 3.js
@@ -6,9 +6,9 @@ var lengthOfLongestSubstring = function(s) {
     let longest = ''
 
     for (let i = 0; i < s.length; i++) {
-        for (let j = i; j < s.length; j++) {
-            const str = s.substring(i, j)
-            const curr = s[j]
+        for (let j = i + 1; j <= s.length; j++) {
+            const str = s.substring(i, j - 1)
+            const curr = s[j - 1]
 
             if (str.includes(curr)) {
                 break;
