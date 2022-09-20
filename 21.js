cfef05a02a1f0c03a179c1f31292762f71500419
mauricioaznar <mauricioaznar94@gmail.com>
2022-09-20 11:37:57 -0500

    created 22.js

--- 21.js
+++ 21.js
@@ -11,17 +11,23 @@
  * @return {ListNode}
  */
 var mergeTwoLists = function(list1, list2) {
+
+    // pointer that will be the new head
     let fakeHead = new ListNode()
-    
+
+    // newHead.next is going to be the smallest value
     let curr = fakeHead
     
     
     let lead1 = list1
     let lead2 = list2
-    
+
+
+    // loop and choose the lowest value and traverse both list until one is finished
     while (lead1 && lead2) {
         
         if (lead1.val <= lead2.val) {
+            // separate the node and add it to the new list without losing the old list
             const temp = lead1
             lead1 = lead1.next 
             temp.next = null
@@ -36,7 +42,8 @@ var mergeTwoLists = function(list1, list2) {
             curr = curr.next
         }
     }
-    
+
+    // traverse remaining nodes
     while (lead1) {
         const temp = lead1
         lead1 = lead1.next
