f834adaa65fe65574fd08a8671acfe0c7634a387
mauricioaznar <mauricioaznar94@gmail.com>
2022-09-28 12:50:36 -0500

    70.js

---
+++ 70.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var climbStairs = function(n) {
+    const map = new Array(n).fill(0)
+
+    map[0] = 1;
+    map[1] = 2
+
+    for (let i = 2; i < n; i++) {
+        map[i] = map[i - 1] + map[i - 2]
+    }
+
+    return map[n - 1];
+};
