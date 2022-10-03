feba80be11d189d3e65cc57b8ab41a7266aad78a
mauricioaznar <mauricioaznar94@gmail.com>
2022-10-03 12:11:25 -0500

    Create 82.js

---
+++ 82.js
@@ -0,0 +1,39 @@
+
+0/5
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var deleteDuplicates = function(head) {
+
+    const fakeHead = new ListNode()
+    fakeHead.next = head
+
+    let prev = fakeHead
+
+    while(prev.next) {
+        console.log(prev.val)
+        if (prev.next && prev.next.next && prev.next.val === prev.next.next.val) {
+            console.log(prev.val, prev.next.val, prev.next.next.val)
+            let replacement = prev.next
+            while (replacement && prev.next.val === replacement.val) {
+                replacement = replacement.next
+            }
+
+            prev.next = replacement
+        } else {
+            prev = prev.next
+        }
+    }
+
+
+
+    return fakeHead.next
+};
